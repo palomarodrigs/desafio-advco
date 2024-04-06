c87f409347c30462e04a9682af5e3cdceadd72dd
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-04-06 18:53:39 -0300

    feat: add categories page

---
+++ src/pages/categories.tsx
@@ -0,0 +1,17 @@
+import { Flex } from '@chakra-ui/layout'
+import { CategoryMenu } from '../components/category-menu'
+import { CategoriesCard } from '../components/categories/categories-card'
+import { ProductQuiz } from '../components/product-quiz'
+
+export function Categories() {
+  return (
+    <Flex direction="column" align="center">
+      <CategoryMenu />
+
+      <Flex direction="column" gap="50px" py={['30px', '60px']}>
+        <CategoriesCard />
+        <ProductQuiz />
+      </Flex>
+    </Flex>
+  )
+}
